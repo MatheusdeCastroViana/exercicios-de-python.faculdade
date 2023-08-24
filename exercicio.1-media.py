9c1f80c756bea0bc317151f64567e26f4531fa31
Matheus de Castro <matheusdecastroviana@gmail.com>
2023-08-24 15:11:02 -0300

    Fim do Programa!
    Finalizei o cálculo de média!

---
+++ exercicio.1-media.py
@@ -0,0 +1,9 @@
+print("Calculo de Média dos alunos")
+print(("-")*40)
+
+n1 = float(input("Digite a sua primeira nota: "))
+n2 = float(input("Digite a sua segunda nota: "))
+
+M = (n1+n2)/2
+
+print(f"Sua Média é: {M}")
